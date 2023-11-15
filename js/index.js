69c5ed6472e7db68ff394675fba380cdb02ac004
Joaco Marina <joacomarina@MacBook-Pro-de-Jorge-Marina-3.local>
2023-11-15 09:05:09 -0300

    js

--- js/index.js
+++ js/index.js
@@ -1,43 +1,43 @@
-let peliculas = document.querySelector(".peliculasp")
+// let peliculas = document.querySelector(".peliculasp")
 
-fetch("https://api.themoviedb.org/3/movie/76341?api_key=514fe6f92e87e8b96d2b08b7f2b1a078")
-    .then(function(response){
-        return response.json()
-    })
+// fetch("https://api.themoviedb.org/3/movie/76341?api_key=514fe6f92e87e8b96d2b08b7f2b1a078")
+//     .then(function(response){
+//         return response.json()
+//     })
 
-    .then(function(data){
-        let info = data.results
-        console.log(info)
+//     .then(function(data){
+//         let info = data.results
+//         console.log(info)
 
-        let contenedorPeli = document.querySelector(".peliculasp")
+//         let contenedorPeli = document.querySelector(".peliculasp")
     
-        for (let i = 0; i < 5; i++){
-            contenedorPeli.innerHTML += 
-            <a href="./detallepelicula.html?id=${data.results[i].id}">
-                <article>
-                <img src="https://image.tmdb.org/t/p/w500/${info[i].poster_path}" alt="pelis"></img>
-                </article>
-                <h5 class="tituPelis">${info[i].title}</h5>
-			    <p class="fecha-estreno">${info[i].release_date}</p>
-            </a>
-        }
+//         for (let i = 0; i < 5; i++){
+//             contenedorPeli.innerHTML += 
+//             <a href="./detallepelicula.html?id=${data.results[i].id}">
+//                 <article>
+//                 <img src="https://image.tmdb.org/t/p/w500/${info[i].poster_path}" alt="pelis"></img>
+//                 </article>
+//                 <h5 class="tituPelis">${info[i].title}</h5>
+// 			    <p class="fecha-estreno">${info[i].release_date}</p>
+//             </a>
+//         }
 
-    .catch (function(error){
-        console.log(error)
-    })
-})
+//     .catch (function(error){
+//         console.log(error)
+//     })
+// })
 
-fetch("https://api.themoviedb.org/3/tv/popular/76341?api_key=514fe6f92e87e8b96d2b08b7f2b1a078")
-    .then(function(response){
-        return responde.json()
-    })
-    .then(function(data){
-        let info = data.results
-        console.log(info)
+// fetch("https://api.themoviedb.org/3/tv/popular/76341?api_key=514fe6f92e87e8b96d2b08b7f2b1a078")
+//     .then(function(response){
+//         return responde.json()
+//     })
+//     .then(function(data){
+//         let info = data.results
+//         console.log(info)
 
-        let contenedorSerie = document.querySelector(".")
+//         let contenedorSerie = document.querySelector(".")
 
-        for(let i = 0; i < info.length; i++) {
-            contenedorSerie.innerHTML += 
-        }
-    })
+//         for(let i = 0; i < info.length; i++) {
+//             contenedorSerie.innerHTML += 
+//         }
+//     })
--- js/search-results.js
+++ js/search-results.js
@@ -1,21 +1,35 @@
 
-let resultadoBusqueda = document.querySelector("#lupa")
 
-let queryBusqueda = location.search
-let queryString = new URLSearchParams(queryBusqueda)
-let peliserie = queryString.get("busqueda")
+let busquedaa = new URLSearchParams(location.search)
+let peliserie = busquedaa.get("busqueda")
+let resultados = document.querySelector(".resultados")
+let contenido = ""
 
-let busquedaResultado = document.querySelector(".resultadobusqueda")
-busquedaResultado.innerHTML = `Resultados de busqueda de: "${query}"`
+let formulario = document.querySelector("form")
 
-
-let apiKey = "1173214cf5e2ac8f2c0ac1c242d0ec8a"
-
-fetch(`https://api.themoviedb.org/3/movie/76341?api_key=${apiKey}&query=${peliserie}`)
-.then(function(data){
-    for(i = 0; i < data.results.length; i++){
-        console.log(data)
-        resultadoBusqueda.innerHTML +=  `<a href="./resultadobusquda.html?i${data.results[i].id}"></a> <article> <img src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}" alt=${data.results[i].title} class="img"></img> </article>
-        
+formulario.addEventListener("submit", function(e){
+    e.preventDefault()
+    let value=input.value.length
+    if(input.value == ""){
+        alert("Este campo es obligatorio")
+    }else if(input.value.length < 3){
+        alert("Este campo tiene que tener al menos 3 caracteres")
+    } else{
+        fetch
     }
-    })
+})
+
+// let busquedaResultado = document.querySelector(".resultadobusqueda")
+// busquedaResultado.innerHTML = `Resultados de busqueda de: "${query}"`
+
+
+// let apiKey = "1173214cf5e2ac8f2c0ac1c242d0ec8a"
+
+// fetch(`https://api.themoviedb.org/3/movie/76341?api_key=${apiKey}&query=${peliserie}`)
+// .then(function(data){
+//     for(i = 0; i < data.results.length; i++){
+//         console.log(data)
+//         resultadoBusqueda.innerHTML +=  `<a href="./resultadobusquda.html?i${data.results[i].id}"></a> <article> <img src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}" alt=${data.results[i].title} class="img"></img> </article>
+        
+//     }
+//     })
