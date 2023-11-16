90551c10f32be007b0b03be51c0e820de5889d12
Lorenzopellegrino <lpellegrino@udesa.edu.ar>
2023-11-16 15:59:57 -0300

    index.js

--- js/index.js
+++ js/index.js
@@ -1,43 +1,79 @@
-// let peliculas = document.querySelector(".peliculasp")
+fetch("https://api.themoviedb.org/3/movie/popular?api_key=bf0e25b4b648e8ee928c7dede4d12427")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        let results = data.results
+        let peliculas = document.querySelector("#pelicula-2023")
 
-// fetch("https://api.themoviedb.org/3/movie/76341?api_key=514fe6f92e87e8b96d2b08b7f2b1a078")
-//     .then(function(response){
-//         return response.json()
-//     })
+        for (let i = 0; i < 7; i++){
+            peliculas.innerHTML += `
+                <a href="./detallepelicula.html?id=${results[i].id}">
+                    <article>
+                        <img src="https://image.tmdb.org/t/p/w500/${results[i].poster_path}" alt="pelis"></img>
+                    </article>
+                    <h5 class="tituPelis">${results[i].title}</h5>
+                    <p class="fecha-estreno">${results[i].release_date}</p>
+                </a>`;
+        }
+    })
+    .catch(function(error){
+        console.log(error);
+    });
 
-//     .then(function(data){
-//         let info = data.results
-//         console.log(info)
+fetch("https://api.themoviedb.org/3/tv/popular?api_key=bf0e25b4b648e8ee928c7dede4d12427")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        let results = data.results
+        let series = document.querySelector("#series")
 
-//         let contenedorPeli = document.querySelector(".peliculasp")
-    
-//         for (let i = 0; i < 5; i++){
-//             contenedorPeli.innerHTML += 
-//             <a href="./detallepelicula.html?id=${data.results[i].id}">
-//                 <article>
-//                 <img src="https://image.tmdb.org/t/p/w500/${info[i].poster_path}" alt="pelis"></img>
-//                 </article>
-//                 <h5 class="tituPelis">${info[i].title}</h5>
-// 			    <p class="fecha-estreno">${info[i].release_date}</p>
-//             </a>
-//         }
+        for (let i = 0; i < 7; i++){
+            series.innerHTML += `
+                <a href="./detalleserie.html?id=${results[i].id}">
+                    <article>
+                        <img src="https://image.tmdb.org/t/p/w500/${results[i].poster_path}" alt="series"></img>
+                    </article>
+                    <h5 class="tituSeries">${results[i].name}</h5>
+                    <p class="fecha-estreno">${results[i].first_air_date}</p>
+                </a>`;
+        }
+    })
+    .catch(function(error){
+        console.log(error);
+    });
 
-//     .catch (function(error){
-//         console.log(error)
-//     })
-// })
+fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=bf0e25b4b648e8ee928c7dede4d12427")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        let results = data.results
+        let populares = document.querySelector("#populares")
 
-// fetch("https://api.themoviedb.org/3/tv/popular/76341?api_key=514fe6f92e87e8b96d2b08b7f2b1a078")
-//     .then(function(response){
-//         return responde.json()
-//     })
-//     .then(function(data){
-//         let info = data.results
-//         console.log(info)
+        for (let i = 0; i < 7; i++){
+            populares.innerHTML += `
+                <a href="./detallepelicula.html?id=${results[i].id}">
+                    <article>
+                        <img src="https://image.tmdb.org/t/p/w500/${results[i].poster_path}" alt="peliculas"></img>
+                    </article>
+                    <h5 class="tituPelis">${results[i].title}</h5>
+                    <p class="fecha-estreno">${results[i].release_date}</p>
+                </a>`;
+        }
+    })
+    .catch(function(error){
+        console.log(error);
+    });
 
-//         let contenedorSerie = document.querySelector(".")
 
-//         for(let i = 0; i < info.length; i++) {
-//             contenedorSerie.innerHTML += 
-//         }
-//     })
+
+
+
+
+
+
